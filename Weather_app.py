3bd887f9a39b9ad09ef7ff7afcd8faf92d8c9eb1
Biltu5 <biltunayak78242372@gmail.com>
2021-01-08 01:09:14 +0530

    Add class Data

--- Weather_app.py
+++ Weather_app.py
@@ -1,16 +1,30 @@
 from tkinter import *
+from PIL import ImageTk,Image
 import weather_data
+from itertools import cycle
+from random import randint
 
 class app:
     '''
     Create full Frame of weather app
     '''
-    def __init__(self,root):
-         ###### Variable Declaration ######
-        self.status = StringVar(value = "Weather in Modha Prodesh")
-        self.current_city = StringVar()
+    def __init__(self,root,image):
+        ###### Variable Declaration ######
+        self.root = root
+        self.image = image
+        obj = weather_data.Data()
+        #obj.get_data('Delhi',3)
+        current = obj.Current_Status('Delhi')
+
+        self.status = StringVar(value = "Weather in Delhi")
+        self.select_city = StringVar()
         self.set_Date = StringVar(value = " 06 JAN,  2021")
         self.set_Time = StringVar(value = " 06.30.43 PM")
+
+        self.country_name = f"{current['location']['name']}, {current['location']['region']}"
+        self.temprature = f"{current['current']['temp_c']}°C"
+        self.current_weather = f"{current['current']['condition']['text']}"
+
         bg_color = 'dodger blue'
         fg_color = 'ghost white'
 
@@ -24,16 +38,19 @@ class app:
         self.top_Frame_Right.place(x=910,y=0, height =100)
 
         self.left_Frame = Frame(root,bg=bg_color)
-        self.left_Frame.place(x=0,y=105,height=495,width =200)
-        self.right_Frame = Frame(root,bg = bg_color)
-        self.right_Frame.place(x=205,y=105,height=495,width =550)
-        self.graph_Frame =Frame(root,bg = bg_color)
-        self.graph_Frame.place(x=760,y=105,height=375,width =285)
+        self.left_Frame.place(x=0,y=105,height=490,width =200)
+
+        self.Heading_Frame = Frame(root,bg = 'blue')
+        self.Heading_Frame.place(x=205,y=105,height=40,width=840)
+        self.middle_Frame = Frame(root,bg = bg_color)
+        self.middle_Frame.place(x=205,y=145,height=450,width =550)
+        self.right_Frame =Frame(root,bg = 'green')
+        self.right_Frame.place(x=755,y=145,height=375,width =290)
 
         ###### Top Frame Left ######
         Label(self.top_Frame_Left_up, text = 'WEATHER REPORT', font = ('algerian', 20, 'bold'), bg = bg_color, fg = fg_color).pack(fill = X)
         Label(self.top_Frame_Left_down, text = 'Search City :', font = ('georgia', 12), bg = bg_color, fg = fg_color).pack(side = LEFT, padx =20)
-        Entry(self.top_Frame_Left_down, textvariable = self.current_city, width = 20, font = ('georgia', 12)).pack(side = LEFT, padx = 5)
+        Entry(self.top_Frame_Left_down, textvariable = self.select_city, width = 20, font = ('georgia', 12)).pack(side = LEFT, padx = 5)
         Button(self.top_Frame_Left_down, text = 'Search', font = ('georgia', 10),bg = 'Forest green', fg = fg_color).pack(side = LEFT,padx = 5)
         Label(self.top_Frame_Left_down,text = '',bg= bg_color).pack(side=LEFT,padx=70)
         Label(self.top_Frame_Left_down, textvariable = self.status, font = ('georgia', 16), bg = 'dodger blue2', fg = fg_color,anchor = E).pack(side = RIGHT)
@@ -42,13 +59,29 @@ class app:
         Label(self.top_Frame_Right, textvariable = self.set_Date,font = ('century', 12, 'bold'), bg = bg_color, fg = fg_color).pack(pady =5, padx = 5)
         Label(self.top_Frame_Right, textvariable = self.set_Time,font = ('century', 12), bg = bg_color, fg = fg_color).pack(pady = 5, padx = 5)
 
+        ###### Heding Frame ######
+        Label(self.Heading_Frame,text = "Reports",font = ('georgia', 15, 'bold'), bg = 'blue', fg = fg_color).pack(fill = BOTH)
+
+        ###### Middle Frame ######
+        #Label(self.middle_Frame,textvariable=self.country_name).pack()
+        no = randint(1,8)
+        self.change_image(image[f'bg{no}'])
+
+    def change_image(self,img):
+        bg_Label = Label(self.middle_Frame,image=img)
+        bg_Label.place(x = 0, y = 0, relwidth = 1, relheight = 1)
+        bg_Label.image = img
+
 if __name__ == '__main__':
     root = Tk()
     root.geometry('1050x600+200+40')
     root.resizable(False,False)
+    image = {}
+    for i in range(1,9):
+        image[f'bg{i}'] = ImageTk.PhotoImage(Image.open(f"E:\\Weather Boarcast [Python]\\Images\\bg{i}.jpg").resize((550,450)))
     #root.wm_iconbitmap("weather.ico")
     root.title("Weather - By B Nayak")
-    obj = app(root)
+    obj = app(root,image)
     print(obj.__doc__)
     root.mainloop()
 
--- weather_data.py
+++ weather_data.py
@@ -1,2 +1,29 @@
 import requests
 import pandas as pd
+
+class Data:
+    def __init__(self):
+        self.key = '147d1642a5254d08b7993928210201'
+
+    def get_data(self,place,day):
+        '''   {place : String, day : must be in range (1 - 3)}    '''
+        url = f"http://api.weatherapi.com/v1/forecast.json?key={self.key}&q={place}&days={day}"
+        data = requests.get(url).json()
+        clear_data = data['forecast']['forecastday']
+        path = "E:\Weather Boarcast [Python]\CSV"
+
+        # Create data Frames & CSV 
+        for i in range(day):
+            day = clear_data[i]['date']
+            pd.DataFrame(clear_data[i]['hour']).to_csv(f'{path}/Hour_day_{day}.csv')
+            pd.DataFrame(clear_data[i]['day'],index=[0]).to_csv(f'{path}/Day_day_{day}.csv')
+            pd.DataFrame(clear_data[i]['astro'],index=[0]).to_csv(f'{path}/Astro_day_{day}.csv')
+
+    def Current_Status(self,place):
+        url = f'http://api.weatherapi.com/v1/current.json?key={self.key}&q={place}'
+        data = requests.get(url).json()
+        return data
+
+if __name__ == '__main__':
+    obj = Data()
+    #print(obj.Current_Status('burdwan'))
